d2bead8379b4477b6006eed58389c4b8b112599e
Ymi-Basha <166015191+Ymi-Basha@users.noreply.github.com>
2024-04-11 23:34:17 +0200

    Create C. Area of a Circle.cpp

---
+++ #include <iostream> #include <bits/C. Area of a Circle.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main()
+{
+    double r,p=3.14159;
+    cout<<fixed<<setprecision(4);
+    cin>>r;
+    cout<<"A="<<r*r*p;
+
+    return 0;
+}
