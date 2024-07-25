4284e6c3c369326ee4045364ce4f18dc99150127
Ymi-Basha <166015191+Ymi-Basha@users.noreply.github.com>
2024-07-25 18:05:45 +0300

    Create U. Codeforces Checking.cpp

---
+++ U. Codeforces Checking.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+//cout<<
+//cin>>
+int main() {
+    ll in;
+    char c;
+    cin>>in;
+    for(ll i=1; i<=in;i++) {
+        cin>>c;
+        if(c=='c'||c=='o'||c=='d'||c=='e'||c=='f'||c=='r'||c=='s')
+            cout<<"YES"<<endl;
+        else
+            cout<<"NO"<<endl;
+    }
+
+
+
+}
