8f6b16f13e56c4e63132962418bcb612a1d0c8cb
Ymi-Basha <166015191+Ymi-Basha@users.noreply.github.com>
2024-07-25 12:41:58 +0300

    Create L2. Draw it.cpp

---
+++ L2. Draw it.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+//cout<<
+//cin>>
+int main() {
+    char s;
+    ll in;
+    cin>>s>>in;
+    for(ll i=1;i<=in;i++) {
+        ll num;
+        cin>>num;
+        for(ll j=1; j<=num;j++) {
+            cout<<s;
+        }
+        cout<<endl;
+    }
+
+}
