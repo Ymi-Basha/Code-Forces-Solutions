286e84c36fd945f05b006fb82c4d5bc1818e1b93
Ymi-Basha <166015191+Ymi-Basha@users.noreply.github.com>
2024-07-25 18:12:13 +0300

    Create X. Easy Problem.cpp

---
+++ X. Easy Problem.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+//cout<<
+//cin>>
+int main() {
+    ll n;
+    cin>>n;
+    if(n>=13)
+        cout<<"YES";
+    else
+        cout<<"NO";
+}
