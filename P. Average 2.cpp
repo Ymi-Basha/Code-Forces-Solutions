b5803a344edc0533847ed0fd1d7f7dd0ab04b7bd
Ymi-Basha <166015191+Ymi-Basha@users.noreply.github.com>
2024-07-25 13:07:02 +0300

    Create P. Average 2.cpp

---
+++ P. Average 2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+//cout<<
+//cin>>
+int main() {
+    double a,b,c,avg;
+    cin>>a>>b>>c;
+    avg=(a*2+b*3+c*5)/10.0;
+    cout<<fixed <<setprecision(1);
+    cout<<"MEDIA = "<<avg*1.0;
+
+}
