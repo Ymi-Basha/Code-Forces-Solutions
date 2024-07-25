dc85c33aa870542d2cda10ef1ace51414fe9b692
Ymi-Basha <166015191+Ymi-Basha@users.noreply.github.com>
2024-07-25 11:58:49 +0300

    Create L1. Summation from 1 to N.cpp

---
+++ L1. Summation from 1 to N.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+//cout<<
+//cin>>
+int main() {
+    ll in, sum;
+    cin>>in;
+    cout<<(in*(in+1))/2;
+
+}
