f02b5b9f9823636bbef300d3b24299939712ee05
Ymi-Basha <166015191+Ymi-Basha@users.noreply.github.com>
2024-07-25 11:09:41 +0300

    Create K. Multiplication Table.cpp

---
+++ K. Multiplication Table.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+//cout<<
+//cin>>
+int main()
+{
+    ll num;
+    cin>>num;
+    for(int i=1;i<=10;i++)
+    {
+        cout<<i<<" x "<<num<<" = "<<i*num<<endl;
+    }
+}
