df0b14a40ebb991d769651f2688234715a62bcad
Ymi-Basha <166015191+Ymi-Basha@users.noreply.github.com>
2024-07-26 14:27:55 +0300

    Create C. Positions.cpp

---
+++ C. Positions.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+//cout<<
+//cin>>
+int main() {
+    int n;
+    cin>>n;
+    int a[n];
+        for(int i=0;i<n;i++) {
+            cin>>a[i];
+            if(a[i]<=10) {
+                cout<<"A["<<i<<"] = "<<a[i]<<endl;
+
+            }
+        }
+
+
+
+}
