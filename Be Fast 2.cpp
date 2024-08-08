4ee54ba54bc6ce3094cb92aacd3ae93576a0aa08
Ymi-Basha <166015191+Ymi-Basha@users.noreply.github.com>
2024-08-08 12:01:54 +0300

    Be Fast 2.cpp

---
+++ Be Fast 2.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+//cout<<
+//cin>>
+
+int main() {
+    ll l,r,n;
+    cin>>l>>r>>n;
+    cout << (r-l+1)-r/n +(l-1)/n << endl;
+}
