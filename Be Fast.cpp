b1769b627e0addfda73a2c07ef451de74b304b3d
Ymi-Basha <166015191+Ymi-Basha@users.noreply.github.com>
2024-08-16 16:03:38 +0300

    Create Be Fast.cpp

---
+++ Be Fast.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+//cout<<
+//cin>>
+int main() {
+    ll n,sum=0;
+    cin>>n;
+    for(ll i=1;i<=n;i++) {
+        sum+=i*i;
+    }
+    cout<<sum;
+}
