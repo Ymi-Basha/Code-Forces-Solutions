30f22aa45f0c789e4a74e6ee8c1138458b333276
Ymi-Basha <166015191+Ymi-Basha@users.noreply.github.com>
2024-08-16 15:57:00 +0300

    Weird Function 2.cpp

---
+++ Weird Function 2.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+//cout<<
+//cin>>
+
+int main() {
+    string s1,s2;
+    cin>>s1>>s2;
+    cout<<s2<<s1<<s1<<s2<<s1<<s2<<s1<<s1<<s2;
+
+    }
