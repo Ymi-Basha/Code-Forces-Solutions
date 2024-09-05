7d84160ec1f5d957d49d0e56455c1e7ce5204dfc
Ymi-Basha <166015191+Ymi-Basha@users.noreply.github.com>
2024-09-05 12:49:49 +0300

    Megiddo.cpp

---
+++ Megiddo.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+//cout<<
+//cin>>
+
+int main() {
+    ll arr[3];
+    cin>>arr[0]>>arr[1]>>arr[2];
+    sort(arr,arr+3);
+    cout<<arr[1];
+}
